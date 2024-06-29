66076dfacc8a200e5aa62b1d5d8845b5cbcb979d
Aditya Uttarwar <136005005+DeAdiU@users.noreply.github.com>
2024-06-29 19:54:22 +0530

    Update page.jsx

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -44,7 +44,7 @@ const Products = () => {
             <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
                 {filteredEntries.map((product) => (
                     <div key={product._id} className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" onClick={() => router.push(`/product/${product._id}`)}>
-                        <a href="#">
+                        <a>
                             <img src={product.imageUrl} alt="Product" className="h-80 w-72 object-cover rounded-t-xl" />
                             <div className="px-4 py-3 w-72">
                                 <span className="text-gray-400 mr-3 uppercase text-xs">{product.categoryId}</span>
